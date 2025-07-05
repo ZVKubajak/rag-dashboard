ac19c61b2f583e4b58eee7736ee66f13d6d01e9f
VanceGoblin <zvkubajak@gmail.com>
2025-07-05 07:33:11 -0500

    Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT!;
+
+app.use(cors());
+app.use(express.json());
+// app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+});
